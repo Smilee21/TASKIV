be9851b63c55acf2f71269c903bde807213cff43
Smilee21 <118314878+Smilee21@users.noreply.github.com>
2023-01-21 01:13:09 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,93 +1,72 @@
-import express from "express";
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
-import indexRoutes from "./routes/index.js";
-import sqlite3 from "sqlite3";
-import nodemailer from "nodemailer";
-import dotenv from "dotenv";
+import express from 'express';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import indexRoutes from './routes/index.js'
+import sqlite3 from 'sqlite3';
+
 let sql;
-dotenv.config()
-// create reusable transporter object using the default SMTP transport
-let transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: "antomigel23@gmail.com",
-    pass: process.env.MAIL_KEY,
-  },
-});
+
 
 const app = express();
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+let array = []
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 //configuraciones
-app.set("views", join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 app.use(express.json());
-app.use(indexRoutes);
+app.use(indexRoutes)
 
-//Cuando se realiza el post  mediante el fecht
+//Cuando se realiza el post  mediante el fech 
 //este a su vez hace la accion de llevar esos datos a la base de datos de SQLite3
-app.post("/contact", async (req, res) => {
-  let elementosForm = req.body;
-  let localizacion = elementosForm.pais;
-  let ipjson = elementosForm.ip;
-  let fecha = elementosForm.fecha;
-  let nombre = elementosForm.nombre;
-  let email = elementosForm.email;
-  let mensaje = elementosForm.mensaje;
-  //send to email
-    await transporter.sendMail({
-    from: '"ANTONIO VOLCAN" <antomigel23@gmail.com>', // sender address
-    to: "antomigel23@dispostable.com",
-    subject: "Alguien quiere contactarse contigo", // Subject line
-    text: `Se a registrado un nuevo usuario 
-              Nombre: ${nombre}
-              Email: ${email}
-              Pais: ${localizacion}
-              Ip: ${ipjson}
-              Fecha: ${fecha}
-              Mensaje del usuario: ${mensaje}`
-              
-  });
+app.post('/contact', (req, res) => {
+    console.log(req.body);
 
-  //aqui se insertan los datos en la tabla
-  sql =
-    "INSERT INTO users (fecha,nombre,email,mensaje,ipjson,localizacion)VALUES (?,?,?,?,?,?)";
-  db.run(sql, [fecha, nombre, email, mensaje, ipjson, localizacion], (err) => {
-    if (err) return console.error(err.message);
-  });
-  //este comando es para conssultar la tabla y mostrarla en la consola
-  // en total hay 7 usuarios de prueba puede que halla mas.
-  //se le pusieron nombres aleatorios
-  sql = "SELECT * FROM users";
-  db.all(sql, [], (err, rows) => {
-    if (err) return console.error(err.message);
-    rows.forEach((row) => {
-      console.log(row);
+    let elementosForm = req.body;
+    let ip = req.ip;
+    let ipjson = ip
+    let fecha = elementosForm.fecha
+    let nombre = elementosForm.nombre
+    let email = elementosForm.email
+    let mensaje = elementosForm.mensaje
+    //aqui se insertan los datos en la tabla
+    sql = ('INSERT INTO users (fecha,nombre,email,mensaje,ipjson)VALUES (?,?,?,?,?)');
+    db.run(sql, [fecha, nombre, email, mensaje,ipjson], (err) => {
+        if (err) return console.error(err.message);
+        
     });
-  });
-  res.send(req.body);
+    //este comando es para conssultar la tabla y mostrarla en la consola
+    // en total hay 7 usuarios de prueba puede que halla mas.
+    //se le pusieron nombres aleatorios
+    sql = 'SELECT * FROM users';
+    db.all(sql, [], (err, rows) => {
+        if (err) return console.error(err.message);
+        rows.forEach(row => {
+            console.log(row)
+        })
+    })
+    res.send(req.body)
+    console.log()
+})
+
+
+const db = new sqlite3.Database('./db/form.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) return console.error(err.message)
 });
-
-const db = new sqlite3.Database(
-  "./db/form.db",
-  sqlite3.OPEN_READWRITE,
-  (err) => {
-    if (err) return console.error(err.message);
-  }
-);
 //se crea la tabla//
-/*  sql = 'ALTER TABLE users ADD localizacion';
-db.run(sql); */
+/* sql = 'CREAte TABLE users(id INTEGER PRIMARY KEY, fecha,nombre,email,mensaje,ipjson)';
+db.run(sql);
+ */
 
-app.use(express.static(join(__dirname, "public")));
 
-const PORT = process.env.PORT || 3000;
-console.log("server on port", PORT);
+
+app.use(express.static(join(__dirname, 'public')))
+
+const PORT = process.env.PORT || 3000
+console.log('server on port', PORT);
 
 app.listen(PORT, () => {
-  console.log("server in port:", PORT);
-});
+    console.log('server in port:', PORT);
+})
